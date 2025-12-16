a940ddaa8f92b15369006ba701dcc1a0c072019f
web124213 <124213@students.ue-varna.bg>
2025-12-16 21:08:02 +0200

    Add files via upload
    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+    // Avatar upload preview
+    document.getElementById('avatarUpload').addEventListener('change', function(e) {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                document.getElementById('avatarPreview').src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+</script>
